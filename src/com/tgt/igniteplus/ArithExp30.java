830d8ca8d614aa5fe2eede8f2f96a44921775eb5
Yashika <yashikabc@gmail.com>
2020-03-18 23:43:26 +0530

    java program

---
+++ src/com/tgt/igniteplus/ArithExp30.java
@@ -0,0 +1,74 @@
+package com.tgt.igniteplus;
+import java.util.*;
+public class ArithExp30 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter an expression:");
+        String exp = in.nextLine();
+        ArithExp30 i = new ArithExp30();
+        System.out.println("The value of expression is " + i.getValue(exp));
+    }
+
+    public int getValue(String expression) {
+        if (expression == null || expression.length() == 0) return 0;
+        Stack<Character> OptrStack = new Stack();
+        Stack<Integer> OpndStack = new Stack<>();
+        int digit = 0;
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (c == ' ') continue;
+            else if (Character.isDigit(c)) {
+                digit = c - '0';  //convert character c to integer
+                while (i + 1 < expression.length() && Character.isDigit(expression.charAt(i + 1))) {
+                    digit = digit * 10 + (expression.charAt(++i) - '0');    //If the value consists of more than one digit
+                }
+                OpndStack.push(digit);
+            } else if (isOpeator(c)) {
+                while (!OptrStack.isEmpty() && hasPrecedence(c, OptrStack.peek())) {
+                    OpndStack.push(applyOperation(OptrStack.pop(), OpndStack.pop(), OpndStack.pop()));
+                }
+                OptrStack.push(c);
+            } else if (c == '(') {
+                OptrStack.push(c);
+            } else if (c == ')') {
+                while (OptrStack.peek() != '(') {
+                    OpndStack.push(applyOperation(OptrStack.pop(), OpndStack.pop(), OpndStack.pop()));
+                }
+                OptrStack.pop();
+            }
+        }
+        while (!OptrStack.isEmpty()) {
+            OpndStack.push(applyOperation(OptrStack.pop(), OpndStack.pop(), OpndStack.pop()));
+        }
+        return OpndStack.pop();
+    }
+
+    public boolean isOpeator(char c) {
+        return (c == '+' || c == '-' || c == '*' || c == '/');
+    }
+
+    public int applyOperation(char optr, int a, int b) {
+        switch (optr) {
+            case '+':
+                return a + b;
+            case '-':
+                return b - a;
+            case '*':
+                return a * b;
+            case '/':
+                return b / a;
+        }
+        return 0;
+    }
+
+    public boolean hasPrecedence(char op1, char op2) {
+        if (op2 == '(' || op2 == ')')
+            return false;
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+            return false;
+        else
+            return true;
+    }
+}
+
+
