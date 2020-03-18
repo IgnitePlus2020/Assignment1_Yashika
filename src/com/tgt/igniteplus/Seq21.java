b7e4049064447685bb07a7ccdf3102092c0b0481
Yashika <yashikabc@gmail.com>
2020-03-18 22:24:46 +0530

    java program

---
+++ src/com/tgt/igniteplus/Seq21.java
@@ -0,0 +1,22 @@
+package com.tgt.igniteplus;
+import java.util.Scanner;
+public class Seq21 {
+    public static void main(String[] args) {
+        int n;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        n = in.nextInt();
+        System.out.println("Series=");
+        int a = 1, b = 0, i = 0;
+        while (b < n) {
+            b = a + (i * i);
+            if (i % 2 != 0)
+                System.out.print(b * (-1) + "\t");
+            else
+                System.out.print(b + "\t");
+            a = b;
+            i += 1;
+        }
+    }
+}
+
